370550ea81c8f958835f3fcca0a748055a8c208b
cAMP <138028480+casakc@users.noreply.github.com>
2025-05-25 18:48:42 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,8 @@
-/*import React, { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Home/Home";
-import Aktuelles from "./Aktuelles/Aktuelles";
-import AktuellesAnswer from "./Aktuelles/AktuellesAnswer";
-
-
-
-const App = () => {
-
-  const [questionId, setQuestionId] = useState(null); // Store ID globally
-  
-  return (
-    <BrowserRouter basename="/">
-      <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/aktuelles" element={<Aktuelles setQuestionId={setQuestionId} />} />
-      <Route path="/answer" element={<AktuellesAnswer questionId={questionId} />} />
-
-
-      </Routes>
-    </BrowserRouter>
-  );
-};
-
-export default App;*/
-
 import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home/Home";
 import Aktuelles from "./Aktuelles/Aktuelles";
 import AktuellesAnswer from "./Aktuelles/AktuellesAnswer"; 
-import RandomAnswer from "./Aktuelles/RandomAnswer";  // ✅ Import your answer component
 
 const App = () => {
   const [questionId, setQuestionId] = useState(null); // Store question ID globally
